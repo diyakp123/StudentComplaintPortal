71a9c81943a5bdfc19070062e498d9bbb6d618e1
diyakp123 <diyap552@gmail.com>
2024-06-15 15:50:37 +0530

    added models in Users app

--- Users/models.py
+++ Users/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 # Create your models here.
-#class registration(models.Model):
+class registration(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name= models.CharField(max_length=30)
+    email = models.EmailField(max_length=35)
+    department_id = models.IntegerField()
+    password = models.CharField(max_length=40)
+    confirm_password = models.CharField(max_length=40)
+
+
+class login(models.Model):
+    email = models.EmailField(max_length=35)
+    password = models.CharField(max_length=40)
+
+
+
 
 
